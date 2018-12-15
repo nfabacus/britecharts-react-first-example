d66b35b07cabd500677cdb62ea6529c2de85b880
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-12-15 07:01:03 +0000

    adds first britecharts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Bar } from 'britecharts-react';
 import './App.css';
 
+const barData = () => [
+  {
+    name: 'Radiating',
+    value: 2,
+  },
+  {
+    name: 'Opalescent',
+    value: 4,
+  },
+  {
+    name: 'Shining',
+    value: 3,
+  },
+  {
+    name: 'Vibrant',
+    value: 6,
+  },
+  {
+    name: 'Vivid',
+    value: 6,
+  },
+  {
+    name: 'Brilliant',
+    value: 1,
+  },
+];
+const marginObject = {
+  left: 100,
+  right: 40,
+  top: 40,
+  bottom: 40
+};
+
 class App extends Component {
+  state = {
+    width: window.innerWidth
+  };
+
+  handleWindowSizeChange = () => {
+    this.setState({
+      width: window.innerWidth
+    })
+  }
+
+  componentWillMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange)
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>BriteCharts-React</h1>
+        <p>How do I get rid of yAxis label line??  API documentation is inconsistent with what actually available as well...</p>
+        <Bar
+          data={barData}
+          isHorizontal={true}
+          isAnimated={true}
+          height={400}
+          width={this.state.width}
+          betweenBarsPadding={0.3}
+          margin={marginObject}
+          yAxisPaddingBetweenChart={0}
+        />
       </div>
     );
   }
